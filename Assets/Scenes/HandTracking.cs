a5b15f860446a1b5de5c77b2e66f1ce0f381681d
John Steven Pineda <jspinedam@gmail.com>
2023-03-08 20:38:41 -0500

    Fisicas

---
+++ Assets/Scenes/HandTracking.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UDPReceive updRecieve;
+    public GameObject[] HandPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string data = updRecieve.data;
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length-1,1);
+        string[] points = data.Split(',');
+        
+        for(int i= 0; i<21;i++)
+        {
+            float x = float.Parse(points[i*3])/100;
+            float y = float.Parse(points[i*3+1])/100;
+            float z = float.Parse(points[i*3*2])/100;
+
+            HandPoints[i].transform.localPosition = new Vector3(x,y,z);
+        }
+    }
+}
